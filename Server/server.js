7d439acfa63d0dcf5c2c5a093d3e9076f337dc74
Ameer Hamza <2022cs17@student.uet.edu.pk>
2025-04-27 02:44:41 +0500

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -17,8 +17,8 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
 
-    // origin: "https://hncloudkitchen.netlify.app", 
-    origin: process.env.CLIENT_URL, 
+    origin: "https://hncloudkitchen.netlify.app", 
+    // origin: process.env.CLIENT_URL, 
 
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
